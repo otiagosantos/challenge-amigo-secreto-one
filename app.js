d16d459467ce06e4ef29249452edd3a1e4208184
Tiago dos Santos <otiagosantos.code@gmail.com>
2025-03-14 19:44:26 -0300

    feat: Adiciona funcionalidade de adicionar amigos a lista.
    Um nome deve ser preenchido no campo, ao clicar no botão "Adicionar", se não estiver vazio, o nome do campo é acrescentado ao array.
    
    Closes #4

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
 let amigos = [];
+
+let amigoInputElement = document.querySelector("#amigo");
+let adicionarAmigoButtonElement = document.querySelector(".input-wrapper .button-add");
+
+function adicionarAmigo() {
+    if(amigoInputElement.value == "") {
+        alert("Por favor, insira um nome.");
+        return;
+    }
+
+    amigos.push(amigoInputElement.value);
+
+    console.log(amigos);
+    amigoInputElement.value = ""
+}
