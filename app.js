f743d45d29fb60e646d38045621c0a9c8314151c
Tiago dos Santos <otiagosantos.code@gmail.com>
2025-03-14 21:10:46 -0300

    feat: Atualiza a DOM com os nomes da lista de amigos.
    Os nomes adicionados ao array de amigos, agora são mostrados na lista não ordenada abaixo do input.
    
    Closes #5

--- app.js
+++ app.js
@@ -4,15 +4,26 @@ let amigos = [];
 
 let amigoInputElement = document.querySelector("#amigo");
 let adicionarAmigoButtonElement = document.querySelector(".input-wrapper .button-add");
+let listaAmigosElement = document.querySelector("#listaAmigos");
 
 function adicionarAmigo() {
-    if(amigoInputElement.value == "") {
+    if (amigoInputElement.value == "") {
         alert("Por favor, insira um nome.");
         return;
     }
 
     amigos.push(amigoInputElement.value);
+    renderizaListaAmigos();
 
     console.log(amigos);
     amigoInputElement.value = ""
+}
+
+function renderizaListaAmigos() {
+    listaAmigosElement.innerHTML = "";
+    for (let i = 0; i < amigos.length; i++) {
+        let item = document.createElement("li");
+        item.innerHTML = amigos[i];
+        listaAmigosElement.appendChild(item);
+    }
 }
